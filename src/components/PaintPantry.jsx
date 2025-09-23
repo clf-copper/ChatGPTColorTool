98cf1d9d3c789e7beb2bc981d66b514a31610431
clf-copper <cybillfoster@gmail.com>
2025-09-23 18:25:23 -0500

    Update PaintPantry.jsx

--- src/components/PaintPantry.jsx
+++ src/components/PaintPantry.jsx
@@ -1,5 +1,5 @@
-
 import React, { useState, useEffect } from "react";
+import paintColors from "chatgptcolortool/src/data/paintColors.json";
 
 export default function PaintPantry({ onSelectPaint }) {
   const [paints, setPaints] = useState([]);
